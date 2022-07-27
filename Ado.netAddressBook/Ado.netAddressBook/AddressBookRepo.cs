5b1e7d5e47bb59a2fcb2c8278ccf51466f550391
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-27 22:23:00 +0530

    [gurpreet] UC1_CreateDBofAddressBook

---
+++ Ado.netAddressBook/Ado.netAddressBook/AddressBookRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY33_ADO.NETAddressBookPP
+{
+    public class AddressBookRepo
+    {
+        public void Create_Database()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection("@Data Source = (localdb)/ProjectModels; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False;");
+                Connection.Open();
+                SqlCommand command = new SqlCommand("Create database Addressbook_ADO;", Connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("AddressbookService Database created successfully!");
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
